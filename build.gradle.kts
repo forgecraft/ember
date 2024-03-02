ab9cf57b828700497b78b8985076edf88903c2a1
Up <upcraftedlp@gmail.com>
2024-03-02 11:53:32 +0100

    set up database and create bootstrap configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,15 +3,21 @@ import org.jetbrains.gradle.ext.runConfigurations
 import org.jetbrains.gradle.ext.settings
 
 plugins {
-    id("java")
-    id("application")
+    java
+    application
     id("org.jetbrains.gradle.plugin.idea-ext") version "1.1.3"
+    id("org.jooq.jooq-codegen-gradle") version "3.19.5"
 }
 
 val javaVersion = 21
 
 group = "net.forgecraft.services"
 version = "1.0.0"
+
+sourceSets {
+    create("generated")
+    create("database")
+}
 
 repositories {
     mavenCentral()
@@ -24,15 +30,36 @@ dependencies {
     implementation("org.javacord:javacord:3.8.0")
 
     implementation("org.apache.logging.log4j:log4j-to-slf4j:2.22.1")
-    implementation("ch.qos.logback:logback-classic:1.5.0")
+    sourceSets.getByName("database").implementationConfigurationName("org.apache.logging.log4j:log4j-to-slf4j:2.22.1")
+
+    runtimeOnly("ch.qos.logback:logback-classic:1.5.0")
+    sourceSets.getByName("database").runtimeOnlyConfigurationName("ch.qos.logback:logback-classic:1.5.0")
 
     // Jackson
     implementation("com.fasterxml.jackson.core:jackson-core:2.16.1")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.16.1")
 
+    implementation("org.jooq:jooq:3.19.5")
+    sourceSets.getByName("database").implementationConfigurationName("org.jooq:jooq:3.19.5")
+
+    runtimeOnly("org.xerial:sqlite-jdbc:3.45.1.0")
+    sourceSets.getByName("database").runtimeOnlyConfigurationName("org.xerial:sqlite-jdbc:3.45.1.0")
+    jooqCodegen("org.xerial:sqlite-jdbc:3.45.1.0")
+
     // Utils
     implementation("commons-io:commons-io:2.15.1")
-    implementation("org.jetbrains:annotations:24.1.0")
+
+    compileOnly("org.jetbrains:annotations:24.1.0")
+    sourceSets.getByName("database").compileOnlyConfigurationName("org.jetbrains:annotations:24.1.0")
+
+}
+
+tasks.withType(JavaCompile::class).configureEach {
+    options.encoding = "UTF-8"
+    options.release.set(javaVersion)
+
+    source(sourceSets.getByName("generated").allJava)
+    source(sourceSets.getByName("database").allJava)
 }
 
 java {
@@ -41,16 +68,73 @@ java {
     }
 }
 
-// Runs for intelij
+jooq {
+    version = "3.19.5"
+
+    configuration {
+
+        jdbc {
+            driver = "org.sqlite.JDBC"
+            url = "jdbc:sqlite:${project.file("run/data/sqlite.db").absolutePath}"
+        }
+
+        generator {
+            database {
+                name = "org.jooq.meta.sqlite.SQLiteDatabase"
+                includes = ".*"
+
+                // exclude the migrations table from schema generation
+                excludes = """
+                    __migrations
+                """.trimIndent()
+            }
+
+            target {
+                packageName = "net.forgecraft.services.ember.db.schema"
+                directory = project.file("src/generated/java").absolutePath
+            }
+        }
+    }
+
+    project.file("run/data").mkdirs()
+}
+
 idea.project.settings {
     runConfigurations {
         register("Start Ember", Application::class) {
+            project.file("run").mkdirs()
             mainClass = "net.forgecraft.services.ember.Main"
             programParameters = "--config=config.json"
-            moduleName = "ember.main"
+            moduleName = "${project.name}.main"
+            workingDirectory = project.file("run").absolutePath
+        }
+
+        register("Bootstrap Database", Application::class) {
+            project.file("run").mkdirs()
+            mainClass = "net.forgecraft.services.ember.db.Main"
+            moduleName = "${project.name}.database"
+            workingDirectory = project.file("run").absolutePath
         }
     }
 }
+
+// configure the java application plugin for CI and anyone not using IDEA
+application {
+    mainClass.set("net.forgecraft.services.ember.Main")
+    executableDir = project.file("run").absolutePath
+}
+
+tasks.named("run", JavaExec::class) {
+    args("--config=config.json")
+}
+
+tasks.register("boostrapDatabase", JavaExec::class) {
+    group = "application"
+    mainClass.set("net.forgecraft.services.ember.db.Main")
+    classpath = sourceSets.getByName("database").runtimeClasspath
+    workingDir = project.file("run")
+}
+tasks.getByName("jooqCodegen").dependsOn("boostrapDatabase")
 
 tasks.test {
     useJUnitPlatform()
---
+++ src/database/java/net/forgecraft/services/ember/db/migration/MigrationHelper.java
@@ -0,0 +1,141 @@
+package net.forgecraft.services.ember.db.migration;
+
+import org.jooq.CloseableDSLContext;
+import org.jooq.impl.DSL;
+import org.jooq.impl.SQLDataType;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.EnumSet;
+import java.util.List;
+import java.util.regex.Pattern;
+
+public class MigrationHelper {
+
+    public static final String MIGRATIONS_TABLE_NAME = "__migrations";
+    public static final Pattern VALID_MIGRATION_NAME = Pattern.compile("\\d{3,}_.+");
+
+    public static void applyPendingMigrations(CloseableDSLContext ctx) {
+
+        // create migrations table programmatically, then apply pending migrations
+        ctx.createTableIfNotExists(MIGRATIONS_TABLE_NAME)
+                .column("id", SQLDataType.INTEGER.identity(true))
+                .column("version", SQLDataType.INTEGER.notNull())
+                .column("name", SQLDataType.VARCHAR(255).notNull())
+                .constraints(
+                        DSL.unique("name"),
+                        DSL.unique("version")
+                )
+                .execute();
+
+        int currentVersion = getCurrentVersion(ctx);
+
+        List<Migration> pendingMigrations;
+
+        try {
+            var rs = MigrationHelper.class.getClassLoader().getResource("_donotremovethisisahack");
+            if(rs == null) {
+                throw new IllegalStateException("unable to find own file path");
+            }
+            var jarRoot = Path.of(rs.toURI()).getParent();
+
+            pendingMigrations = getPendingMigrations(currentVersion, jarRoot.resolve("migrations"));
+            //TODO logging
+            System.out.println("Found " + pendingMigrations.size() + " pending migrations.");
+        } catch (IOException e) {
+            // TODO logging
+            throw new RuntimeException("unable to apply migrations", e);
+        } catch (URISyntaxException e) {
+            // should NEVER happen
+            throw new RuntimeException(e);
+        }
+
+        for (Migration migration : pendingMigrations) {
+            try {
+                ctx.transaction(trx -> {
+                    var up = migration.getUp();
+                    if (up == null) {
+                        //TODO logging and skip?
+                        throw new IllegalStateException("Up migration not found for " + migration.name());
+                    }
+
+                    for (String statement : up) {
+                        trx.dsl().execute(statement);
+                    }
+
+                    trx.dsl()
+                            .insertInto(DSL.table(MIGRATIONS_TABLE_NAME), DSL.field("version"), DSL.field("name"))
+                            .values(migration.version(), migration.name())
+                            .execute();
+                });
+            } catch (RuntimeException e) { // DataAccessException
+                throw new RuntimeException("Failed to apply migration " + migration.name(), e);
+            }
+        }
+    }
+
+    private static List<Migration> getPendingMigrations(int currentVersion, Path migrationsDir) throws IOException {
+        var all = parseMigrations(migrationsDir);
+        //TODO debug logging
+        System.out.println("Found " + all.size() + " migrations.");
+
+        return all.stream().filter(migration -> migration.test(currentVersion)).sorted().toList();
+    }
+
+    private static int getCurrentVersion(CloseableDSLContext ctx) {
+
+        var result = ctx.select(DSL.max(DSL.field("version"))).from(MIGRATIONS_TABLE_NAME).fetchOne();
+
+        if (result == null) {
+            return -1;
+        }
+
+        Integer max = (Integer) result.value1();
+
+        return max != null ? max : -1;
+    }
+
+    public static List<Migration> parseMigrations(Path migrationsDir) throws IOException {
+        if (!Files.isDirectory(migrationsDir, LinkOption.NOFOLLOW_LINKS)) {
+            //TODO logging
+            System.err.println("Migrations directory not found at " + migrationsDir.toAbsolutePath());
+            return List.of();
+        }
+        var migrationsDirName = migrationsDir.getFileName().toString();
+
+        List<Migration> migrations = new ArrayList<>();
+        Files.walkFileTree(migrationsDir, EnumSet.noneOf(FileVisitOption.class), 2, new SimpleFileVisitor<>() {
+
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                var dirName = dir.getFileName().toString();
+
+                // skip the root dir
+                if(dirName.equals(migrationsDirName)) {
+                    return FileVisitResult.CONTINUE;
+                }
+
+
+                if (!VALID_MIGRATION_NAME.asMatchPredicate().test(dirName)) {
+                    return FileVisitResult.SKIP_SUBTREE;
+                }
+
+                try {
+                    var migrationVersion = Integer.parseInt(dirName.substring(0, dirName.indexOf('_')), 10);
+                    migrations.add(new Migration(dirName, migrationVersion, dir));
+                } catch (NumberFormatException e) {
+                    return FileVisitResult.SKIP_SUBTREE;
+                }
+
+                return FileVisitResult.CONTINUE;
+            }
+        });
+
+        Collections.sort(migrations);
+        return migrations;
+    }
+}
