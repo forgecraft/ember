224933948836c16159543fc47ccd15869e8d9590
Up <upcraftedlp@gmail.com>
2024-03-11 07:27:47 +0100

    Revert "a"
    This reverts commit 1191c96b1a701ad2c93509fd5c6d536e20bdb6b7.

--- src/test/java/net/forgecraft/services/ember/mods/downloader/ModInfoParserTest.java
+++ src/test/java/net/forgecraft/services/ember/mods/downloader/ModInfoParserTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 
 import java.nio.file.Path;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ModInfoParserTest {
     @Test
     void canParseValidForgeMod() {
